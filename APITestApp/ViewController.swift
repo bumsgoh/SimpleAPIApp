5e1e130ef6055c960ca7eec1b25625747b372b5c
bumslap <bumslap@naver.com>
2018-07-24 13:35:09 +0900

    Finished UI config, API Method setting

--- APITestApp/ViewController.swift
+++ APITestApp/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var requestBtn: UIButton!
     @IBOutlet var responsedTextLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         UISetUp()
@@ -19,25 +20,36 @@ class ViewController: UIViewController {
     func UISetUp() {
         self.requestBtn = {
             let button: UIButton = UIButton()
-            button.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("Pressed to get API Infos", for: UIControlState.normal)
+            button.setTitleColor(UIColor.blue, for: UIControlState.normal)
             button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-            button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30).isActive = true
+            button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+            button.addTarget(self, action: #selector(requestCurrentTime), for: UIControlEvents.touchUpInside)
             return button
         }()
+        
         self.responsedTextLabel = {
             let label: UILabel = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
             self.view.addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            
             label.topAnchor.constraint(equalTo: self.requestBtn.bottomAnchor, constant: 20).isActive = true
             label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            label.text = "API Info"
             return label
         }()
     }
     
     @IBAction func requestCurrentTime() {
+        do {
+        guard let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/currentTime") else {fatalError()}
+        let response = try String(contentsOf: url)
         
-        
+        self.responsedTextLabel.text = response
+        self.responsedTextLabel.sizeToFit()
+        } catch let e as Error {print(e.localizedDescription)}
     }
 }
 
